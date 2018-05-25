00ae94d344a420fc644b7b5bc8c81a781ecf2c6d
lcdsr <lcds.rafael@gmail.com>
2018-05-25 12:10:27 +0200

    creation du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { ProductsComponent } from './modules/products/products.component';
+import { ProductListComponent } from './modules/products/product-list/product-list.component';
+import { ProductEditComponent } from './modules/products/product-edit/product-edit.component';
+import { ProductDetailComponent } from './modules/products/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductEditComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
