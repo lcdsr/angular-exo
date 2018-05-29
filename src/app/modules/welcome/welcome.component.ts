fcdd72bf4dccb8dfcdcc2a7eebea1b5aebb739bc
lcdsr <Cronos@Rafaels-MacBook-Pro.local>
2018-05-29 07:36:53 +0200

    welcome page modification
    rajout de gifs qui change au click avec un randomizer

--- src/app/modules/welcome/welcome.component.ts
+++ src/app/modules/welcome/welcome.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent  {
 
-  constructor() { }
+  static GIFS: string[] = [
+    'https://media.giphy.com/media/IrQcyTog3X8VW/giphy.gif',
+    'https://media.giphy.com/media/ue4rk7zGOW2Qg/giphy.gif',
+    'https://media.giphy.com/media/13q7CYNKuDhliE/giphy.gif',
+    'https://media.giphy.com/media/xTiTntH1cWq5gyBceQ/giphy.gif'
+  ];
 
-  ngOnInit() {
+  public currentGif: string;
+
+  constructor() {
+    this.generateRandomGif();
+  }
+
+  private generateRandomGif(): void {
+    const max = WelcomeComponent.GIFS.length;
+    const gifIndex = Math.ceil(Math.random() * max - 1);
+    this.currentGif = WelcomeComponent.GIFS[gifIndex];
   }
 
 }
